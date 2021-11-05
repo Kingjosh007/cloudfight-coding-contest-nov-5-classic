7b98b0119b0e291d1d2c3ea4d1afecc22d2485e2
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-11-05 17:29:30 +0100

    Level 1-1 solved

--- level-1/code.js
+++ level-1/code.js
@@ -1,1 +1,20 @@
-console.log(1);
+// using fs, read the content of level1_1.in file and store it in an array
+
+const fs = require('fs');
+const file = fs.readFileSync('./level-1/level1_1.in', 'utf8');
+const arr = file.split('\n');
+
+const input = arr.slice(1);
+const inputAsLine = input.reduce((t, e) => t + e, '');
+
+// split with white space
+const words = inputAsLine.split(/\s+/)
+                         .filter(w => !['start', 'print', 'end'].includes(w))
+
+const solution = words.join('');
+
+console.log(solution);
+
+
+
+
